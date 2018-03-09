137e27a3afd8a2a582db96721e8a722c843143b8
prathamApp <prathambaner123@gmail.com>
2018-03-09 12:15:25 +0530

    ketan
    Updates

---
+++ app/src/main/java/com/example/pravin/angreziok/activities/StartMenu.java
@@ -0,0 +1,15 @@
+package com.example.pravin.angreziok.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pravin.angreziok.R;
+
+public class StartMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_menu);
+    }
+}
