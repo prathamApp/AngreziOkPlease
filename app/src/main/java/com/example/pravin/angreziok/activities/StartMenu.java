59acfa1cbe215e785335b6bc705b08c431e7b855
prathamApp <prathambaner123@gmail.com>
2018-03-09 15:01:41 +0530

    Ketan
    Update

--- app/src/main/java/com/example/pravin/angreziok/activities/StartMenu.java
+++ app/src/main/java/com/example/pravin/angreziok/activities/StartMenu.java
@@ -2,10 +2,17 @@ package com.example.pravin.angreziok.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.pravin.angreziok.R;
 
+import butterknife.BindView;
+
 public class StartMenu extends AppCompatActivity {
+
+    @BindView(R.id.nav_options)
+    RecyclerView nav_options;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
