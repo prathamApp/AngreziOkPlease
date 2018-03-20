08198f5cc85d0c19ddf7113a932ccb4ba69cd5a0
prathamApp <prathambaner123@gmail.com>
2018-03-20 16:42:15 +0530

    pk change

---
+++ app/src/main/java/com/example/pravin/angreziok/ui/jod_tod_round/jod_tod_fragments/Jod_tod_round_one.java
@@ -0,0 +1,32 @@
+package com.example.pravin.angreziok.ui.jod_tod_round.jod_tod_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pravin.angreziok.BaseFragment;
+
+/**
+ * Created by Pravin on 20/03/2018.
+ */
+
+public class Jod_tod_round_one extends BaseFragment {
+    
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
