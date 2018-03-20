1b41c49b5c414ff544351a3c3af95d2462b78e5a
prathamApp <prathambaner123@gmail.com>
2018-03-20 11:36:49 +0530

    pk commit

---
+++ app/src/main/java/com/example/pravin/angreziok/ui/jod_tod_round/JodTodRound.java
@@ -0,0 +1,11 @@
+package com.example.pravin.angreziok.ui.jod_tod_round;
+
+import com.example.pravin.angreziok.BaseActivity;
+
+/**
+ * Created by Pravin on 20/03/2018.
+ */
+
+public class JodTodRound extends BaseActivity implements JodTodContract.JodTodView {
+
+}
