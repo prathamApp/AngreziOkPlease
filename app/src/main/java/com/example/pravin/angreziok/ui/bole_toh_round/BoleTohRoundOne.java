7d445a3826922965a8f3ce42634dfd51a93dab92
prathamApp <prathambaner123@gmail.com>
2018-03-23 14:47:17 +0530

    my changes pk

--- app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleTohRoundOne.java
+++ app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleTohRoundOne.java
@@ -7,19 +7,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-
+import android.widget.Toast;
 import com.example.pravin.angreziok.BaseFragment;
 import com.example.pravin.angreziok.R;
+import com.github.anastr.flattimelib.CountDownTimerView;
+import com.github.anastr.flattimelib.intf.OnTimeFinish;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BoleTohRoundOne extends BaseFragment implements BoleTohContract.BoleTohRoundOneView{
 
+    @BindView(R.id.mCountDownTimer)
+    CountDownTimerView mCountDownTimer;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
     }
 
     @Override
@@ -32,7 +36,58 @@ public class BoleTohRoundOne extends BaseFragment implements BoleTohContract.Bol
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        setOnClickListeners();
+        startTimer(60000);
     }
+
+    private void setOnClickListeners() {
+
+        mCountDownTimer.setOnTimeFinish(new OnTimeFinish() {
+            @Override
+            public void onFinish() {
+                Toast.makeText(getActivity(), "finish", Toast.LENGTH_SHORT).show();
+                mCountDownTimer.success();
+            }
+        });
+
+        mCountDownTimer.setOnEndAnimationFinish(new OnTimeFinish() {
+            @Override
+            public void onFinish() {
+            }
+        });
+
+    }
+
+    public void pauseTimer() {
+        mCountDownTimer.pause();
+    }
+
+    public void continueTimer() {
+        mCountDownTimer.resume();
+    }
+
+    public void stopTimer() {
+        mCountDownTimer.stop();
+    }
+
+    public void startTimer(long time) {
+            mCountDownTimer.start(time);
+    }
+
+    public void displaySuccess() {
+        mCountDownTimer.success();
+    }
+
+    public void displayFailed() {
+        mCountDownTimer.failure();
+    }
+
+    public void readyTimer() {
+        mCountDownTimer.ready();
+    }
+
 }
 
 
