a436d27ebbc26150df61d2ecb82949aec19568b0
prathamApp <prathambaner123@gmail.com>
2018-03-26 11:11:11 +0530

    Ketan

--- app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleTohRoundOne.java
+++ app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleTohRoundOne.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import com.example.pravin.angreziok.BaseFragment;
 import com.example.pravin.angreziok.R;
@@ -21,6 +24,20 @@ public class BoleTohRoundOne extends BaseFragment implements BoleTohContract.Bol
 
     @BindView(R.id.mCountDownTimer)
     CountDownTimerView mCountDownTimer;
+    @BindView(R.id.ll_mic)
+    LinearLayout layout_mic;
+    @BindView(R.id.iv_image1)
+    ImageView iv_image1;
+    @BindView(R.id.iv_image2)
+    ImageView iv_image2;
+    @BindView(R.id.iv_image3)
+    ImageView iv_image3;
+    @BindView(R.id.iv_image4)
+    ImageView iv_image4;
+    @BindView(R.id.ib_speaker)
+    ImageButton ib_speaker;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,6 +77,11 @@ public class BoleTohRoundOne extends BaseFragment implements BoleTohContract.Bol
             }
         });
 
+    }
+
+    @Override
+    public void doInitalWork() {
+        
     }
 
     @Override
