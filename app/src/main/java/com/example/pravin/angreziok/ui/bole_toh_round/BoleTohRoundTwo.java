9faba9c6f0a90a22cd98e701f2585465dd52b11b
prathamApp <prathambaner123@gmail.com>
2018-03-27 14:33:10 +0530

    pk

--- app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleTohRoundTwo.java
+++ app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleTohRoundTwo.java
@@ -2,11 +2,9 @@ package com.example.pravin.angreziok.ui.bole_toh_round;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.pravin.angreziok.BaseFragment;
