bdc2a1c67bd9c8aada6a91f94d7836245b94955a
prathamApp <prathambaner123@gmail.com>
2018-04-02 10:07:05 +0530

    ketan46

--- app/src/main/java/com/example/pravin/angreziok/modalclasses/PlayerModal.java
+++ app/src/main/java/com/example/pravin/angreziok/modalclasses/PlayerModal.java
@@ -1,12 +1,41 @@
 package com.example.pravin.angreziok.modalclasses;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Ameya on 28-Mar-18.
  */
 
-public class PlayerModal {
+public class PlayerModal implements Parcelable {
 
     public String studentID,studentName,studentScore,studentAlias;
+
+    public PlayerModal(String studentID, String studentName, String studentScore, String studentAlias) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.studentScore = studentScore;
+        this.studentAlias = studentAlias;
+    }
+
+    protected PlayerModal(Parcel in) {
+        studentID = in.readString();
+        studentName = in.readString();
+        studentScore = in.readString();
+        studentAlias = in.readString();
+    }
+
+    public static final Creator<PlayerModal> CREATOR = new Creator<PlayerModal>() {
+        @Override
+        public PlayerModal createFromParcel(Parcel in) {
+            return new PlayerModal(in);
+        }
+
+        @Override
+        public PlayerModal[] newArray(int size) {
+            return new PlayerModal[size];
+        }
+    };
 
     public String getStudentID() {
         return studentID;
@@ -38,5 +67,18 @@ public class PlayerModal {
 
     public void setStudentAlias(String studentAlias) {
         this.studentAlias = studentAlias;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(studentID);
+        dest.writeString(studentName);
+        dest.writeString(studentScore);
+        dest.writeString(studentAlias);
     }
 }
