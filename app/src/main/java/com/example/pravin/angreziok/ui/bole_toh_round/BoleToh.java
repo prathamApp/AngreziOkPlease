ad7798396b5032d6ca5997b2a81443dcbd156873
prathamApp <prathambaner123@gmail.com>
2018-04-07 15:29:28 +0530

    ketan

--- app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleToh.java
+++ app/src/main/java/com/example/pravin/angreziok/ui/bole_toh_round/BoleToh.java
@@ -7,24 +7,40 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.pravin.angreziok.BaseActivity;
 import com.example.pravin.angreziok.R;
 import com.example.pravin.angreziok.animations.MyBounceInterpolator;
 import com.example.pravin.angreziok.contentplayer.TextToSpeechCustom;
 import com.example.pravin.angreziok.modalclasses.PlayerModal;
+import com.example.pravin.angreziok.ui.GifView;
 import com.example.pravin.angreziok.util.PD_Utility;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class BoleToh extends BaseActivity implements BoleTohContract.BoleTohView {
 
     BoleTohContract.BoleTohPresenter presenter;
     public static TextToSpeechCustom playtts;
     static ArrayList<PlayerModal> playerModalArrayList;
+
+    @BindView(R.id.tv_instructions)
+    TextView tv_instructions;
+    @BindView(R.id.btn_skip_instructions)
+    Button btn_skip;
+    @BindView(R.id.ib_replay_instructions)
+    LinearLayout btn_replay;
+    @BindView(R.id.iv_anupam_gif)
+    GifView anupamGif;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +58,21 @@ public class BoleToh extends BaseActivity implements BoleTohContract.BoleTohView
 
         presenter = new BoleTohPresenterImpl(this);
         playtts = new TextToSpeechCustom(this, 1.0f);
+
+        showInstructions();
+
+
         //loadFragment(1);
+    }
+
+    @OnClick(R.id.btn_skip_instructions)
+    public void startGame(){
+        loadFragment(1);
+    }
+
+
+    private void showInstructions() {
+
     }
 
     @Override
