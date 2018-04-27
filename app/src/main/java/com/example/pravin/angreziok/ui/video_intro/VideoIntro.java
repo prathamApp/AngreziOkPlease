0c3ad9a1c24726602644e7b04a5840487370c010
prathamApp <prathambaner123@gmail.com>
2018-04-27 16:55:43 +0530

    pk commits

--- app/src/main/java/com/example/pravin/angreziok/ui/video_intro/VideoIntro.java
+++ app/src/main/java/com/example/pravin/angreziok/ui/video_intro/VideoIntro.java
@@ -1,13 +1,18 @@
 package com.example.pravin.angreziok.ui.video_intro;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.VideoView;
 
 import com.example.pravin.angreziok.BaseActivity;
 import com.example.pravin.angreziok.R;
+import com.example.pravin.angreziok.database.AppDatabase;
+import com.example.pravin.angreziok.database.BackupDatabase;
 import com.example.pravin.angreziok.ui.start_menu.QRActivity;
 import com.example.pravin.angreziok.util.PD_Utility;
 
@@ -21,7 +26,7 @@ public class VideoIntro extends BaseActivity implements VideoIntroContract.Video
     VideoView videoView;
 
     String videoPath;
-
+    private AppDatabase appDatabase;
     VideoIntroContract.VideoIntroPresenter presenter;
 
     @Override
@@ -29,11 +34,23 @@ public class VideoIntro extends BaseActivity implements VideoIntroContract.Video
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_intro);
         ButterKnife.bind(this);
+        appDatabase = Room.databaseBuilder(this,
+                AppDatabase.class, AppDatabase.DB_NAME)
+                .build();
         presenter = new VideoIntroPresenterImpl(this, this, videoView);
         videoPath = PD_Utility.getExternalPath(this) + "Videos/intro.mp4";
         PD_Utility.showLog("ext_path::", videoPath);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         presenter.playVideo(Uri.parse(videoPath));
+
+        new AsyncTask<Object, Void, Object>() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Log.d(":::",""+appDatabase.getCrlDao().getAllCrls().size());
+                BackupDatabase.backup(VideoIntro.this);
+                return null;
+            }
+        }.execute();
     }
 
     @OnClick(R.id.skip_button)
