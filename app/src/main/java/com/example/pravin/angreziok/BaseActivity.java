4491a47146c3d20b3451ed8e518b064215218c9e
prathamApp <prathambaner123@gmail.com>
2018-05-03 11:37:54 +0530

    pk commits

--- app/src/main/java/com/example/pravin/angreziok/BaseActivity.java
+++ app/src/main/java/com/example/pravin/angreziok/BaseActivity.java
@@ -1,14 +1,18 @@
 package com.example.pravin.angreziok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.pravin.angreziok.contentplayer.TextToSpeechCustom;
+import com.example.pravin.angreziok.modalclasses.PlayerModal;
 import com.example.pravin.angreziok.services.STTService;
 import com.example.pravin.angreziok.services.TTSService;
 import com.example.pravin.angreziok.ui.bole_toh_round.BoleToh;
+import com.example.pravin.angreziok.ui.start_data_confirmation.DataConfirmation;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 /**
@@ -19,6 +23,7 @@ public class BaseActivity extends AppCompatActivity {
 
     public static TTSService ttsService;
     public static STTService sttService;
+    static ArrayList<PlayerModal> playerModalArrayList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +33,17 @@ public class BaseActivity extends AppCompatActivity {
         ttsService.setSpeechRate(0.7f);
         ttsService.setLanguage(new Locale("hi", "IN"));
         sttService = STTService.init(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+        Intent dataConfirmationIntent = new Intent(this, DataConfirmation.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList("studentList", playerModalList);
+        dataConfirmationIntent.putExtras(bundle);
+        startActivity(dataConfirmationIntent);
     }
 
     @Override
